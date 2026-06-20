73cb92c3f7c93e3e733d34154ac0a35b324ea0b2
dperille <dannyperille@gmail.com>
2026-06-20 08:46:45 -0500

    smaller header on sm to fix voting overlap

--- app/web-readerboard/src/features/MainPage.tsx
+++ app/web-readerboard/src/features/MainPage.tsx
@@ -22,19 +22,22 @@ export default function MainPage() {
   return (
     <div className="h-screen max-w-8xl mx-auto p-4">
       <div className="flex h-full flex-col gap-4 lg:flex-row">
-        {/* Ranking Area */}
-        <div className="flex min-h-0 flex-2 flex-col space-y-4 items-center">
-          <ThemeSwitch />
-          <div className="text-center">
-            <h1
-              className="text-3xl font-bold hover:text-muted-foreground cursor-pointer"
-              onClick={() => navigate("/")}
-            >
-              📚 Readerboard
-            </h1>
-            <p className="text-muted-foreground">
-              Rank books head-to-head and build a personalized leaderboard.
-            </p>
+        {/* Ranking area - includes header to give leaderboard more prominence */}
+        <div className="flex min-h-0 flex-2 flex-col space-y-4">
+          {/* Header */}
+          <div className="flex md:flex-col items-center justify-between">
+            <ThemeSwitch />
+            <div className="text-center">
+              <h1
+                className="text-3xl font-bold hover:text-muted-foreground cursor-pointer"
+                onClick={() => navigate("/")}
+              >
+                📚 Readerboard
+              </h1>
+              <p className="hidden md:flex text-muted-foreground">
+                Rank books head-to-head and build a personalized leaderboard.
+              </p>
+            </div>
           </div>
 
           <div className="w-full max-w-4xl lg:max-h-[80vh] lg:items-center lg:justify-center flex-1 lg:py-10">
