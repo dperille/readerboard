afd83408358a50811faf8f3b0c841b37a8382377
dperille <dannyperille@gmail.com>
2026-06-21 10:10:58 -0500

    add skip button

--- app/web-readerboard/src/features/voting/VotingArea.tsx
+++ app/web-readerboard/src/features/voting/VotingArea.tsx
@@ -1,7 +1,7 @@
 import { type Matchup, wasmInstance, type BookId } from "@/types/wasm";
 import BookVoteCard from "./BookVoteCard";
 import { useEffect, useState } from "react";
-import { Delete, Swords } from "lucide-react";
+import { Delete, FastForward, Swords } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 
@@ -74,24 +74,33 @@ export default function VotingArea({
           onRemoveBook={() => removeBook(matchup.bookA.bookId)}
         />
 
-        <div className="flex flex-row items-center justify-center gap-2 md:flex-col">
-          <Button
-            variant="ghost"
-            disabled={!hasUndo}
-            onClick={handleUndo}
-            className={cn(
-              "ring-muted-foreground/20 hover:ring-muted-foreground/40 hover:bg-muted/30 w-14 border border-dashed ring-1 transition-all dark:ring-2",
-              // Diagonal lines if not available
-              hasUndo
-                ? ""
-                : "bg-[repeating-linear-gradient(135deg,rgba(120,120,120,0.25)_0px,rgba(120,120,120,0.25)_4px,transparent_4px,transparent_8px)] dark:bg-[repeating-linear-gradient(135deg,rgba(120,120,120,0.75)_0px,rgba(120,120,120,0.75)_4px,transparent_4px,transparent_8px)]",
-            )}
-          >
-            <Delete className="h-6! w-6!" />
-          </Button>
-          <div className="bg-background hidden flex-col items-center rounded-full px-3 py-2 md:flex">
+        <div className="flex flex-row items-center gap-2 md:flex-col">
+          <div className="bg-background hidden flex-1 flex-col items-center justify-end rounded-full px-3 py-2 md:flex">
             <Swords size={20} />
             <p className="text-muted-foreground text-xs tracking-widest">VS</p>
+          </div>
+          <div className="flex flex-1 justify-center md:flex-col md:justify-end gap-2">
+            <Button
+              variant="ghost"
+              onClick={getMatchup}
+              className="ring-muted-foreground/20 hover:ring-muted-foreground/40 hover:bg-muted/30 w-14 border border-dashed ring-1 transition-all dark:ring-2"
+            >
+              <FastForward className="h-6! w-6!" />
+            </Button>
+            <Button
+              variant="ghost"
+              disabled={!hasUndo}
+              onClick={handleUndo}
+              className={cn(
+                "ring-muted-foreground/20 hover:ring-muted-foreground/40 hover:bg-muted/30 w-14 border border-dashed ring-1 transition-all dark:ring-2",
+                // Diagonal lines if not available
+                hasUndo
+                  ? ""
+                  : "bg-[repeating-linear-gradient(135deg,rgba(120,120,120,0.25)_0px,rgba(120,120,120,0.25)_4px,transparent_4px,transparent_8px)] dark:bg-[repeating-linear-gradient(135deg,rgba(120,120,120,0.75)_0px,rgba(120,120,120,0.75)_4px,transparent_4px,transparent_8px)]",
+              )}
+            >
+              <Delete className="h-6! w-6!" />
+            </Button>
           </div>
         </div>
 
